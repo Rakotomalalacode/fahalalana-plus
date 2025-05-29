349dce03586f43bf29e1e801ce11701327434990
R-code <rakotomalalacode@gmail.com>
2025-05-29 16:54:28 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { withAuth } from "next-auth/middleware"
 export default withAuth(
   function middleware(req) {
     // Lógica adicional si es necesaria
+    signIn: '/auth/signin',
   },
   {
     callbacks: {
