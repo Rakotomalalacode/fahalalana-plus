203d77ec9d7cc5c396d5679c80649ec5920b68db
Rakotomalalacode <rakotomalalacode@gmail.com>
2025-05-30 05:55:58 +0300

    loaging modifer

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import AuthErrorClient from "./AuthErrorClient";
+import Loading from "@/components/loading/Loading";
 
 export default function AuthErrorPage() {
   return (
-    <Suspense fallback={<div>Chargement...</div>}>
+    <Suspense fallback={<div className="h-screen w-screen"><Loading /></div>}>
       <AuthErrorClient />
     </Suspense>
   );
--- app/auth/manual-register/page.tsx
+++ app/auth/manual-register/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react"
 import ManualRegisterClient from "./ManualRegisterForm"
+import Loading from "@/components/loading/Loading"
 
 export default function ManualRegisterPage() {
   return (
-    <Suspense fallback={<div>Chargement...</div>}>
+    <Suspense fallback={<div className="h-screen w-screen"><Loading /></div>}>
       <ManualRegisterClient />
     </Suspense>
   )
